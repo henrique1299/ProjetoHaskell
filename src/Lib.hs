35328bcf28c1ff9d48874fd1bfa9dffa951f1bb2
henrique1299 <80115091+henrique1299@users.noreply.github.com>
2023-08-24 19:48:10 -0300

    first commit

---
+++ src/Lib.hs
@@ -0,0 +1,18 @@
+import Snap
+
+site :: Snap ()
+site =
+    ifTop (writeBS "hello world") <|>
+    route [ ("foo", writeBS "bar")
+          , ("echo/:echoparam", echoHandler)
+          ] <|>
+    dir "static" (serveDirectory ".")
+
+echoHandler :: Snap ()
+echoHandler = do
+    param <- getParam "echoparam"
+    maybe (writeBS "must specify echo/param in URL")
+          writeBS param
+
+main :: IO ()
+main = quickHttpServe site
